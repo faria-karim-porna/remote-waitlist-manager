39104722a4031ae1a8ed38a39d910c6a0b0602d9
faria-karim-porna <faria.porna.08@gmail.com>
2024-11-29 11:36:37 +0000

    Added Try Catch

--- backend/dist/controllers/usersController.js
+++ backend/dist/controllers/usersController.js
@@ -8,17 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.getUser = exports.checkInUser = exports.joinUser = void 0;
 const countOrPositionHelper_1 = require("../helpers/countOrPositionHelper");
@@ -26,53 +15,63 @@ const enums_1 = require("../dataTypes/enums");
 const scheduleService_1 = require("../services.ts/scheduleService");
 const userRepository_1 = require("../repositories/userRepository");
 const joinUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const totalSeatsCount = 10;
-    const { name, partySize } = req.body;
-    const allUserInfo = yield userRepository_1.UserRepository.findAll();
-    const bookedSeatsCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.BookedSeats);
-    const canCheckInSeatsCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.CanCheckInSeats);
-    const usersInWaitingListCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.UsersInWaiting);
-    const availableSeatsCount = totalSeatsCount - (bookedSeatsCount + canCheckInSeatsCount);
-    const isSeatAvailable = partySize <= availableSeatsCount;
-    const isNoUserInWaiting = usersInWaitingListCount === 0;
-    let newUser = {
-        name: name,
-        partySize: partySize,
-    };
-    const canSeatIn = isSeatAvailable && isNoUserInWaiting;
-    if (canSeatIn) {
-        newUser = Object.assign(Object.assign({}, newUser), { status: enums_1.EnumStatus.SeatIn });
-        yield userRepository_1.UserRepository.createUser(newUser);
-        res.status(201).json({ message: "New user has been added", user: newUser });
-        (0, scheduleService_1.runScheduleService)(name, partySize);
+    try {
+        const totalSeatsCount = 10;
+        const { name, partySize } = req.body;
+        const allUserInfo = yield userRepository_1.UserRepository.findAll();
+        const bookedSeatsCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.BookedSeats);
+        const canCheckInSeatsCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.CanCheckInSeats);
+        const usersInWaitingListCount = (0, countOrPositionHelper_1.calculateCount)(allUserInfo, enums_1.EnumCount.UsersInWaiting);
+        const availableSeatsCount = totalSeatsCount - (bookedSeatsCount + canCheckInSeatsCount);
+        const isSeatAvailable = partySize <= availableSeatsCount;
+        const isNoUserInWaiting = usersInWaitingListCount === 0;
+        let newUser = {
+            name: name,
+            partySize: partySize,
+        };
+        const canSeatIn = isSeatAvailable && isNoUserInWaiting;
+        if (canSeatIn) {
+            newUser = Object.assign(Object.assign({}, newUser), { status: enums_1.EnumStatus.SeatIn });
+            yield userRepository_1.UserRepository.createUser(newUser);
+            res.status(201).json({ message: "New user has been added", user: newUser });
+            (0, scheduleService_1.runScheduleService)(name, partySize);
+        }
+        else {
+            newUser = Object.assign(Object.assign({}, newUser), { status: enums_1.EnumStatus.InWaitingList, canCheckIn: false });
+            yield userRepository_1.UserRepository.createUser(newUser);
+            const waitingPosition = yield (0, countOrPositionHelper_1.getUserWaitingPositionByName)(name);
+            newUser = Object.assign(Object.assign({}, newUser), { waitingPosition: waitingPosition });
+            res.status(201).json({ message: "New user has been added", user: newUser });
+        }
     }
-    else {
-        const waitingListLastPosition = (yield userRepository_1.UserRepository.findByData({ status: enums_1.EnumStatus.InWaitingList, canCheckIn: false })).length;
-        newUser = Object.assign(Object.assign({}, newUser), { status: enums_1.EnumStatus.InWaitingList, canCheckIn: false, waitingPosition: waitingListLastPosition + 1 });
-        const { waitingPosition } = newUser, userWithoutPosition = __rest(newUser, ["waitingPosition"]);
-        yield userRepository_1.UserRepository.createUser(userWithoutPosition);
-        res.status(201).json({ message: "New user has been added", user: newUser });
+    catch (error) {
+        res.status(500).json({ message: "Failed to add user", error: error });
     }
 });
 exports.joinUser = joinUser;
 const checkInUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    const { name } = req.body;
-    const user = yield userRepository_1.UserRepository.findByName(name);
-    if (user) {
-        user.status = enums_1.EnumStatus.SeatIn;
-        yield userRepository_1.UserRepository.updateUser(user);
-        res.status(200).send({ message: "User has checked in", user: user });
-        (0, scheduleService_1.runScheduleService)(name, (_a = user.partySize) !== null && _a !== void 0 ? _a : 0);
+    try {
+        const { name } = req.body;
+        const user = yield userRepository_1.UserRepository.findByName(name);
+        if (user) {
+            user.status = enums_1.EnumStatus.SeatIn;
+            yield userRepository_1.UserRepository.updateUser(user);
+            res.status(200).send({ message: "User has checked in", user: user });
+            (0, scheduleService_1.runScheduleService)(name, (_a = user.partySize) !== null && _a !== void 0 ? _a : 0);
+        }
+        else {
+            res.status(404).send({ message: "User not found" });
+        }
     }
-    else {
-        res.status(404).send({ message: "User not found" });
+    catch (error) {
+        res.status(500).json({ message: "Failed to check in user", error: error });
     }
 });
 exports.checkInUser = checkInUser;
 const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { name } = req.params;
     try {
+        const { name } = req.params;
         const userInfo = yield userRepository_1.UserRepository.findByName(name);
         let user = {};
         if (userInfo) {
@@ -91,13 +90,18 @@ const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 });
 exports.getUser = getUser;
 const deleteUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { name } = req.body;
-    const user = yield userRepository_1.UserRepository.deleteUserByName(name);
-    if (user) {
-        res.status(200).send({ message: "User has been deleted" });
+    try {
+        const { name } = req.body;
+        const user = yield userRepository_1.UserRepository.deleteUserByName(name);
+        if (user) {
+            res.status(200).send({ message: "User has been deleted" });
+        }
+        else {
+            res.status(404).send({ message: "User not found" });
+        }
     }
-    else {
-        res.status(404).send({ message: "User not found" });
+    catch (error) {
+        res.status(500).json({ message: "Failed to delete user", error: error });
     }
 });
 exports.deleteUser = deleteUser;
--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -6,60 +6,79 @@ import { runScheduleService } from "../services.ts/scheduleService";
 import { UserRepository } from "../repositories/userRepository";
 
 const joinUser = async (req: Request, res: Response) => {
-  const totalSeatsCount = 10;
-  const { name, partySize }: { name: string; partySize: number } = req.body;
+  try {
+    const totalSeatsCount = 10;
+    const { name, partySize }: { name: string; partySize: number } = req.body;
 
-  const allUserInfo = await UserRepository.findAll();
+    const allUserInfo = await UserRepository.findAll();
+    const bookedSeatsCount = calculateCount(allUserInfo, EnumCount.BookedSeats);
+    const canCheckInSeatsCount = calculateCount(allUserInfo, EnumCount.CanCheckInSeats);
+    const usersInWaitingListCount = calculateCount(allUserInfo, EnumCount.UsersInWaiting);
 
-  const bookedSeatsCount = calculateCount(allUserInfo, EnumCount.BookedSeats);
-  const canCheckInSeatsCount = calculateCount(allUserInfo, EnumCount.CanCheckInSeats);
-  const usersInWaitingListCount = calculateCount(allUserInfo, EnumCount.UsersInWaiting);
+    const availableSeatsCount = totalSeatsCount - (bookedSeatsCount + canCheckInSeatsCount);
+    const isSeatAvailable = partySize <= availableSeatsCount;
+    const isNoUserInWaiting = usersInWaitingListCount === 0;
 
-  const availableSeatsCount = totalSeatsCount - (bookedSeatsCount + canCheckInSeatsCount);
-  const isSeatAvailable = partySize <= availableSeatsCount;
-  const isNoUserInWaiting = usersInWaitingListCount === 0;
-  let newUser: User = {
-    name: name,
-    partySize: partySize,
-  };
-  const canSeatIn = isSeatAvailable && isNoUserInWaiting;
-  if (canSeatIn) {
-    newUser = { ...newUser, status: EnumStatus.SeatIn };
-    await UserRepository.createUser(newUser);
-    res.status(201).json({ message: "New user has been added", user: newUser });
-    runScheduleService(name, partySize);
-  } else {
-    const waitingListLastPosition = (await UserRepository.findByData({ status: EnumStatus.InWaitingList, canCheckIn: false })).length;
-    newUser = { ...newUser, status: EnumStatus.InWaitingList, canCheckIn: false, waitingPosition: waitingListLastPosition + 1 };
-    const { waitingPosition, ...userWithoutPosition } = newUser;
-    await UserRepository.createUser(userWithoutPosition);
-    res.status(201).json({ message: "New user has been added", user: newUser });
+    let newUser: User = {
+      name: name,
+      partySize: partySize,
+    };
+
+    const canSeatIn = isSeatAvailable && isNoUserInWaiting;
+
+    if (canSeatIn) {
+      newUser = { ...newUser, status: EnumStatus.SeatIn };
+      await UserRepository.createUser(newUser);
+      res.status(201).json({ message: "New user has been added", user: newUser });
+      runScheduleService(name, partySize);
+    } else {
+      newUser = {
+        ...newUser,
+        status: EnumStatus.InWaitingList,
+        canCheckIn: false,
+      };
+      await UserRepository.createUser(newUser);
+
+      const waitingPosition = await getUserWaitingPositionByName(name);
+      newUser = { ...newUser, waitingPosition: waitingPosition };
+
+      res.status(201).json({ message: "New user has been added", user: newUser });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to add user", error: error });
   }
 };
 
 const checkInUser = async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const user = await UserRepository.findByName(name);
-  if (user) {
-    user.status = EnumStatus.SeatIn;
-    await UserRepository.updateUser(user);
-    res.status(200).send({ message: "User has checked in", user: user });
-    runScheduleService(name, user.partySize ?? 0);
-  } else {
-    res.status(404).send({ message: "User not found" });
+  try {
+    const { name } = req.body;
+    const user = await UserRepository.findByName(name);
+
+    if (user) {
+      user.status = EnumStatus.SeatIn;
+      await UserRepository.updateUser(user);
+      res.status(200).send({ message: "User has checked in", user: user });
+      runScheduleService(name, user.partySize ?? 0);
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to check in user", error: error });
   }
 };
 
 const getUser = async (req: Request<{ name: string }>, res: Response): Promise<any> => {
-  const { name } = req.params;
   try {
+    const { name } = req.params;
     const userInfo = await UserRepository.findByName(name);
     let user: User = {};
+
     if (userInfo) {
       user = userInfo.toObject() as User;
     }
     const waitingPosition = await getUserWaitingPositionByName(name);
     user = { ...user, waitingPosition: waitingPosition };
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -71,12 +90,17 @@ const getUser = async (req: Request<{ name: string }>, res: Response): Promise<a
 };
 
 const deleteUser = async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const user = await UserRepository.deleteUserByName(name);
-  if (user) {
-    res.status(200).send({ message: "User has been deleted" });
-  } else {
-    res.status(404).send({ message: "User not found" });
+  try {
+    const { name } = req.body;
+    const user = await UserRepository.deleteUserByName(name);
+
+    if (user) {
+      res.status(200).send({ message: "User has been deleted" });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user", error: error });
   }
 };
 
