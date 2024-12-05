d4d89f0e31a7deba7ff79fbadae5ff851036f6a9
faria-karim-porna <faria.porna.08@gmail.com>
2024-12-05 11:51:52 +0000

    Added Test Cases For Join And Check In API

--- backend/src/__test__/backend.test.ts
+++ backend/src/__test__/backend.test.ts
@@ -1,25 +1,36 @@
 import mongoose from "mongoose";
-import supertest from "supertest";
-import { app, EnumStatus, mongoUri, UsersList } from "../server";
+import { EnumStatus } from "../dataTypes/enums";
+import { UsersList } from "../models/usersModel";
+import request from "supertest";
+import { app } from "../server";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { IUser } from "../dataTypes/interfaces";
 
-const request = supertest(app);
+let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
-  // Connect to a test MongoDB database
-  await mongoose.connect(mongoUri ?? "");
+  if (mongoose.connection.readyState !== 0) {
+    await mongoose.disconnect();
+  }
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
 });
 
 afterAll(async () => {
-  // Cleanup the database and disconnect
   await UsersList.deleteMany({});
-  await mongoose.connection.close();
+  await mongoose.disconnect();
+  await mongoServer.stop();
 });
 
-describe("User API", () => {
-  let testUserId: string;
+const changeData = async (name: string, changeData: Partial<IUser>) => {
+  console.log("it has been called with", changeData);
+  await UsersList.findOneAndUpdate({ name }, changeData);
+};
 
+describe("User Join API", () => {
   it("should create a new user with status seat in", async () => {
-    const response = await request.post("/api/join").send({
+    const response = await request(app).post("/api/join").send({
       name: "Faria Karim",
       partySize: 10,
     });
@@ -29,11 +40,10 @@ describe("User API", () => {
     expect(response.body.user.name).toBe("Faria Karim");
     expect(response.body.user.partySize).toBe(10);
     expect(response.body.user.status).toBe(EnumStatus.SeatIn);
-    // testUserId = response.body._id;
   });
 
   it("should create a new user with status in waiting list 1", async () => {
-    const response = await request.post("/api/join").send({
+    const response = await request(app).post("/api/join").send({
       name: "Valerio Donati",
       partySize: 5,
     });
@@ -45,11 +55,10 @@ describe("User API", () => {
     expect(response.body.user.status).toBe(EnumStatus.InWaitingList);
     expect(response.body.user.waitingPosition).toBe(1);
     expect(response.body.user.canCheckIn).toBe(false);
-    // testUserId = response.body._id;
   });
 
   it("should create a new user with status in waiting list 2", async () => {
-    const response = await request.post("/api/join").send({
+    const response = await request(app).post("/api/join").send({
       name: "Daniel Lizik",
       partySize: 5,
     });
@@ -61,11 +70,10 @@ describe("User API", () => {
     expect(response.body.user.status).toBe(EnumStatus.InWaitingList);
     expect(response.body.user.waitingPosition).toBe(2);
     expect(response.body.user.canCheckIn).toBe(false);
-    // testUserId = response.body._id;
   });
 
   it("should create a new user with status in waiting list 3", async () => {
-    const response = await request.post("/api/join").send({
+    const response = await request(app).post("/api/join").send({
       name: "Akash Gupta",
       partySize: 5,
     });
@@ -77,36 +85,23 @@ describe("User API", () => {
     expect(response.body.user.status).toBe(EnumStatus.InWaitingList);
     expect(response.body.user.waitingPosition).toBe(3);
     expect(response.body.user.canCheckIn).toBe(false);
-    // testUserId = response.body._id;
   });
+});
 
-  //   it("should fetch all users", async () => {
-  //     const response = await request.get("/api/users");
-  //     expect(response.status).toBe(200);
-  //     expect(Array.isArray(response.body)).toBe(true);
-  //     expect(response.body.length).toBeGreaterThan(0);
-  //   });
+describe("User Check In API", () => {
+  it("should checkin a user and status will be seat in", async () => {
+    await changeData("Valerio Donati", { canCheckIn: true });
+    // const updatedUser = await UsersList.findOne({ name: "Valerio Donati" });
+    // expect(updatedUser?.canCheckIn).toBe(true);
+    const response = await request(app).post("/api/checkIn").send({
+      name: "Valerio Donati",
+    });
 
-  //   it("should fetch a user by ID", async () => {
-  //     const response = await request.get(`/api/users/${testUserId}`);
-  //     expect(response.status).toBe(200);
-  //     expect(response.body._id).toBe(testUserId);
-  //   });
-
-  //   it("should update a user by ID", async () => {
-  //     const response = await request.put(`/api/users/${testUserId}`).send({
-  //       name: "Jane Doe",
-  //     });
-  //     expect(response.status).toBe(200);
-  //     expect(response.body.name).toBe("Jane Doe");
-  //   });
-
-  //   it("should delete a user by ID", async () => {
-  //     const response = await request.delete(`/api/users/${testUserId}`);
-  //     expect(response.status).toBe(200);
-  //     expect(response.body.message).toBe("User deleted successfully");
-
-  //     const fetchResponse = await request.get(`/api/users/${testUserId}`);
-  //     expect(fetchResponse.status).toBe(404);
-  //   });
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("User has checked in");
+    expect(response.body.user.name).toBe("Valerio Donati");
+    expect(response.body.user.partySize).toBe(5);
+    expect(response.body.user.canCheckIn).toBe(true);
+    expect(response.body.user.status).toBe(EnumStatus.SeatIn);
+  });
 });
