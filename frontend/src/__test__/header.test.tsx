82e6afa9b0dc3d4bed26a716d54db393347ad2c3
faria-karim-porna <faria.porna.08@gmail.com>
2024-12-06 02:39:14 +0000

    Added Username In Header With Test

--- frontend/src/__test__/header.test.tsx
+++ frontend/src/__test__/header.test.tsx
@@ -1,6 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Header } from "../components/common/header";
 import { LogoSVG } from "../assets/svg/logoSvg";
+import { Provider } from "react-redux";
+import { AppStore } from "../components/core/redux/store";
+import { UserAction } from "../components/core/redux/slices/userSlice";
 
 jest.mock("../assets/svg/logoSvg", () => ({
   LogoSVG: jest.fn(() => <svg data-testid="logo-svg">Mock Logo</svg>),
@@ -8,7 +11,11 @@ jest.mock("../assets/svg/logoSvg", () => ({
 
 describe("Header Component", () => {
   it("renders the header with the correct structure and classes", () => {
-    const { container } = render(<Header />);
+    const { container } = render(
+      <Provider store={AppStore}>
+        <Header />
+      </Provider>
+    );
     const headerDiv = container.querySelector(".header");
 
     expect(headerDiv).toBeInTheDocument();
@@ -16,17 +23,36 @@ describe("Header Component", () => {
   });
 
   it("renders the inner div with the correct classes", () => {
-    const { container } = render(<Header />);
+    const { container } = render(
+      <Provider store={AppStore}>
+        <Header />
+      </Provider>
+    );
     const innerDiv = container.querySelector(".d-flex.w-75.align-items-center.position-relative");
 
     expect(innerDiv).toBeInTheDocument();
   });
 
   it("renders the LogoSVG component", () => {
-    const { getByTestId } = render(<Header />);
+    const { getByTestId } = render(
+      <Provider store={AppStore}>
+        <Header />
+      </Provider>
+    );
     const logoSVG = getByTestId("logo-svg");
 
     expect(logoSVG).toBeInTheDocument();
     expect(LogoSVG).toHaveBeenCalled();
+  });
+
+  it("show the username", () => {
+    AppStore.dispatch(UserAction.setUserInfo({ name: "John Doe" }));
+    render(
+      <Provider store={AppStore}>
+        <Header />
+      </Provider>
+    );
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
   });
 });
--- frontend/src/components/common/header.tsx
+++ frontend/src/components/common/header.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { LogoSVG } from "../../assets/svg/logoSvg";
+import { useAppSelector } from "../core/redux/store";
+import { shallowEqual } from "react-redux";
 
 const HeaderComponent = () => {
+  const store = useAppSelector(
+    (state) => ({
+      user: state.user.userInfo,
+    }),
+    shallowEqual
+  );
   return (
     <div className="header w-100 d-flex justify-content-center align-items-center fixed-top">
-      <div className="d-flex w-75 align-items-center position-relative">
+      <div className="d-flex w-75 align-items-center position-relative justify-content-between">
         <LogoSVG />
+        <div className="text-white font-22 font-weight-500 username">{store.user?.name}</div>
       </div>
     </div>
   );
